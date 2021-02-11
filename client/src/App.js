73905d5182ab5c26d812a907a82f8793945f2051
Erick Gonzalez <ericksinbox@gmail.com>
2021-02-11 17:48:00 -0500

    api data added. cors issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,45 @@
 import './App.css';
+import axios from 'axios'
 import { useState, useEffect } from 'react';
 
-function url(path) {
-  return process.env.NODE_ENV === "development" ?
-  `http://localhost:1234${path}` : path
-}
+// function url(path) {
+//   return process.env.NODE_ENV === "development" ?
+//   `http://localhost:1234${path}` : path
+
+
+// }
+const friends = ['76561197970485587', '76561198315462253']
+const myKey = 'C5FF546B0E2C25F709888DE7C081ABE9'
+
 
 function App() {
-  const [data, setData] = useState("Do you see this?")
+  const [data, setData] = useState([])
+  const [friendIDs, setFriendIDs] = useState(friends)
+
 
   useEffect(() => {
-
-    fetch(url("/api/"))
+    axios.get(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${myKey}&steamids=${friendIDs}`)
     .then(res => {
-      res.json()
+      setData(data)
+      
     })
-    .then(apiData => {
-      setData(apiData.data)
+    .catch(err => {
+      console.log(err)
     })
-  }, []);
+  },[])
+
+  // useEffect(() => {
+
+  //   fetch(url("/api/"))
+  //   .then(res => {
+  //     res.json()
+  //   })
+  //   .then(apiData => {
+  //     setData(apiData.data)
+  //   })
+  // }, []);
+
+  
 
 
 
