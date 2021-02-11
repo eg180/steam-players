7b97cccb8cc74582544e4406beec400ce555729f
Erick Gonzalez <ericksinbox@gmail.com>
2021-02-11 16:20:41 -0500

    updated server

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const dotenv = require('dotenv').config()
 const express = require('express');
+const cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 8000
-// app.use(json());
+
+app.use(cors());
+app.use(express.json()); // only necessary when receiving JSON - not outputting (req)
 
 
 app.use("/", (_, res) => {
